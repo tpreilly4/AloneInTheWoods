d4573e3449ca361e4af430d62238b8f044b1325f
tpreilly4 <tpreilly4@gmail.com>
2017-10-19 19:07:26 -0400

    Now There's an Enemy!
    Added LookAtAndFollow script. Self explanatory.
    
    Added low poly Zombie prefab as the 'enemy'.
    
    Fixed lack of HMD alias script. Main reason for this was to be able to attach HMD as a target for LookAtAndFollow script.
    
    Adjusted tree count, added rocks.
    Played with terrain levels. Used raised terrain as pseudo terrain border. For the immersion!
    
    Added capsule colliders to both tree models as well as rock models. No more walking through trees.
    
    Added directional "Light for development Purposes".
    
    *Known fixes needed*
    
    Adjust tree colliders to work more smoothly with VRTK movement. Should probably make them smaller to avoid the too-often black screen/push-back.
    
    Modify LookAtAndFollow.cs to affix the objects "face" to be parallel with the terrain/origin plane. Currently, when 3d model gets too close, it will face upwards to more literally face the HMD alias.
    
    Needs actual gameplay.

---
+++ AloneInTheWoods/Assets/Scripts/ApplyHMDScripts.cs
@@ -0,0 +1,63 @@
+﻿/* This functionality could probably be written as an extension to VRTK_SDKManager and VRTK_SDKSetup,
+ * but both of those classes are sealed and I do not want to awaken any demons which may result of
+ * unsealing those classes. - Luke 
+ *
+ * Courtesy of Luke Tomkus */
+
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+/// <summary>
+/// ApplyHMDScripts makes the defined GameObject HMDScriptContainer a child of VRTK_SDKSetup's actualHeadset object.
+/// You should add this after the VRTK_SDKSetup in the script execution order.
+/// </summary>
+public class ApplyHMDScripts : MonoBehaviour {
+
+    [Tooltip("This is the GameObject containing the scripts you would like to attach to this particular SDKSetup's HMD.")]
+    public GameObject HMDScriptContainer;
+
+    private GameObject ActualHeadset;
+
+	void Start()
+    {
+        if (ActualHeadset == null)
+        {
+            ActualHeadset = this.gameObject.GetComponent<VRTK_SDKSetup>().actualHeadset;
+        }
+        SetupHeadset();
+    }
+
+    private void SetupHeadset()
+    {
+        //this code is ripped straight from VRTK_Setup. All credit there.
+        Action<GameObject, GameObject> setParent = (scriptAliasGameObject, actualGameObject) =>
+        {
+            if (scriptAliasGameObject == null)
+            {
+                return;
+            }
+
+            Transform scriptAliasTransform = scriptAliasGameObject.transform;
+            Transform actualTransform = actualGameObject.transform;
+
+            if (scriptAliasTransform.parent != actualTransform)
+            {
+                Vector3 previousScale = scriptAliasTransform.localScale;
+                scriptAliasTransform.SetParent(actualTransform);
+                scriptAliasTransform.localScale = previousScale;
+            }
+
+            scriptAliasTransform.localPosition = Vector3.zero;
+            scriptAliasTransform.localRotation = Quaternion.identity;
+        };
+
+        if (ActualHeadset != null)
+        {
+            setParent(HMDScriptContainer, ActualHeadset);
+        }
+    }
+}
---
+++ AloneInTheWoods/Assets/Scripts/LookAtAndFollow.cs
@@ -0,0 +1,55 @@
+﻿/* LookAtAndFollow.cs
+ * 
+ * When attached to a GameObject, allows GameObject to follow a target while its z
+ * axis remains fixed on public Transform target, "mTarget". 
+ * Object stops at EPSILON to avoid undesirable collision with HMD alias.
+ * Moves in world space rather than local space, to prevent object from floating.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtAndFollow : MonoBehaviour {
+
+    public Transform followsTarget;
+
+    public float followSpeed = 1.5f;
+
+    public float keepsDistance = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //get our forward in world space
+        Vector3 my_forward = transform.forward;
+
+        //clear the y so we dont move up or down
+        my_forward.y = 0;
+
+        //normalize it, so we have a unit vector even after chopping out the y bit
+        my_forward = my_forward.normalized;
+
+        //scale it by the amount we want to move
+        my_forward *= followSpeed * Time.deltaTime;
+
+        //forces gameobject to face target
+        transform.LookAt(followsTarget.position);
+
+        //do the translation in world space, as long as object is not too close
+     
+        if ((transform.position - followsTarget.position).magnitude > keepsDistance)
+        {
+            transform.Translate(my_forward, Space.World);
+        }
+        else
+        {
+            transform.Translate(0.0f, 0.0f, 0.0f);
+        }
+    }
+}
